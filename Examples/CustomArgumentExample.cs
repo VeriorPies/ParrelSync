3e30fb99c344804426f3ac5b24f55b53a2d5e2ac
DESKTOP-ESM69JQ\ubnnjh <ian121363@gmail.com>
2020-05-09 01:05:06 +0800

    Add custom argument example script.

---
+++ Examples/CustomArgumentExample.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParrelSync.Example
+{
+    public class CustomArgumentExample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (ClonesManager.IsClone())
+            {
+                Debug.Log("This is a clone project.");
+
+                string customArgument = ClonesManager.GetArgument();
+                Debug.Log("The custom argument of this clone project is: " + customArgument);
+                //Do what ever you need with the argument string
+            }
+            else
+            {
+                Debug.Log("This is the original project.");
+            }
+        }
+    }
+}
+#endif
