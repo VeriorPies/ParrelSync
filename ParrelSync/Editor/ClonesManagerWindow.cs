a36a2dbe127f0ca2817a0f308c7ef72064c17931
DESKTOP-ESM69JQ\ubnnjh <ian121363@gmail.com>
2020-05-09 13:40:13 +0800

    Add version update checker.

--- ParrelSync/Editor/ClonesManagerWindow.cs
+++ ParrelSync/Editor/ClonesManagerWindow.cs
@@ -27,7 +27,7 @@ namespace ParrelSync
             get { return ClonesManager.GetCloneProjectsPath().Count >= 1; }
         }
 
-        [MenuItem("ParrelSync/Clones Manager")]
+        [MenuItem("ParrelSync/Clones Manager", priority = 0)]
         private static void InitWindow()
         {
             ClonesManagerWindow window = (ClonesManagerWindow)EditorWindow.GetWindow(typeof(ClonesManagerWindow));
---
+++ ParrelSync/Editor/UpdateChecker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+namespace ParrelSync.Update
+{
+    public class UpdateChecker
+    {
+        const string LocalVersionFilePath = "Assets/ParrelSync/VERSION.txt";
+        const string RemoteVersionURL = "https://raw.githubusercontent.com/314pies/ParrelSync/master/VERSION.txt";
+        [MenuItem("ParrelSync/Check for update", priority = 20)]
+        static void CheckForUpdate()
+        {
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                try
+                {
+                    string localVersionText = AssetDatabase.LoadAssetAtPath<TextAsset>(LocalVersionFilePath).text;
+                    Debug.Log("Local version text : " + localVersionText);
+
+                    string latesteVersionText = client.DownloadString(RemoteVersionURL);
+                    //string versionCheck = client.DownloadString("https://raw.githubusercontent.com/314pies/ParrelSync/master/VERSI");
+                    Debug.Log("latest version text got: " + latesteVersionText);
+                    string messageBody = "Current Version: " + localVersionText +"\n"
+                                         +"Latest Version: " + latesteVersionText + "\n";
+                    if (float.Parse(latesteVersionText) > float.Parse(localVersionText))
+                    {
+                        Debug.Log("There's a newer version");
+                        messageBody += "There's a newer version available";
+                        if(EditorUtility.DisplayDialog("Check for update.", messageBody, "Get latest release", "Close"))
+                        {
+                            Application.OpenURL("https://github.com/314pies/ParrelSync/releases");
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Current version is up-to-date.");
+                        messageBody += "Current version is up-to-date.";
+                        EditorUtility.DisplayDialog("Check for update.", messageBody,"OK");
+                    }
+                    
+                }
+                catch (Exception exp)
+                {
+                    Debug.LogError("Error with checking update. Exception: " + exp);
+                    EditorUtility.DisplayDialog("Update Error","Error with checking update. \nSee console fore more details.",
+                     "OK"
+                    );
+                }
+            }
+        }
+    }
+}
