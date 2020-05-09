f1a58785367f5c06f4d842e4e8b18ba30a0e16ed
DESKTOP-ESM69JQ\ubnnjh <ian121363@gmail.com>
2020-05-09 14:38:29 +0800

    Add non-core UI (Feed back popup, GitHub home and issue page)

---
+++ ParrelSync/Editor/NonCore/OtherMenuItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ParrelSync.NonCore
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class OtherMenuItem
+    {
+        [MenuItem("ParrelSync/GitHub/View this project on GitHub", priority = 10)]
+        private static void OpenGitHub()
+        {
+            Application.OpenURL("https://github.com/314pies/ParrelSync");
+        }
+        [MenuItem("ParrelSync/GitHub/View Issues", priority = 10)]
+        private static void OpenGitHubIssues()
+        {
+            Application.OpenURL("https://github.com/314pies/ParrelSync/issues");
+        }
+    }
+}
