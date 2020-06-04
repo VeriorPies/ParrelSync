793b8b7ed8b4e29722804e3b95fa6e9ffa6c43a2
DESKTOP-ESM69JQ\ubnnjh <ian121363@gmail.com>
2020-06-04 18:27:47 +0800

    Add preference

---
+++ ParrelSync/Editor/Preference.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ParrelSync
+{
+    public class Preference : EditorWindow
+    {
+        [MenuItem("ParrelSync/Preferences", priority = 1)]
+        private static void InitWindow()
+        {
+            Preference window = (Preference)EditorWindow.GetWindow(typeof(Preference));
+            window.titleContent = new GUIContent(ClonesManager.ProjectName + " Preferences");
+            window.Show();
+        }
+
+        const string AssetModProtectionKey = "ParrelSync_AssetModProtect";
+        bool EnableAssetModProtection { get { return EditorPrefs.GetBool(AssetModProtectionKey, true); } }
+        private void OnGUI()
+        {
+            GUILayout.BeginVertical("HelpBox");
+            GUILayout.Label("Preferences");
+            GUILayout.BeginVertical("GroupBox");
+            bool IsAssetModProtectionEnabled = EnableAssetModProtection;
+            IsAssetModProtectionEnabled = GUILayout.Toggle(IsAssetModProtectionEnabled,
+                "Disable asset modification saving in clone editors.");
+            if(IsAssetModProtectionEnabled != EnableAssetModProtection)
+            {
+                EditorPrefs.SetBool(AssetModProtectionKey, IsAssetModProtectionEnabled);
+                Debug.Log("ParrelSync editor preference updated. EnableAssetModProtection: " 
+                    + EnableAssetModProtection);
+            }
+            GUILayout.EndVertical();
+            if (GUILayout.Button("Clear Preferences"))
+            {
+                EditorPrefs.DeleteKey(AssetModProtectionKey);
+                Debug.Log("Removed editor preference: " + AssetModProtectionKey);
+            }
+            GUILayout.EndVertical();
+        }
+    }
+}
