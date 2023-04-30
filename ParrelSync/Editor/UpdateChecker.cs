141b4e1e31966242a6fb7c495b52ac3d1221bc75
314pies <ian121363@gmail.com>
2023-04-29 20:26:22 -0700

    Update version number

--- ParrelSync/Editor/UpdateChecker.cs
+++ ParrelSync/Editor/UpdateChecker.cs
@@ -9,7 +9,7 @@ namespace ParrelSync.Update
     public class UpdateChecker
     {
         //const string LocalVersionFilePath = "Assets/ParrelSync/VERSION.txt";
-        public const string LocalVersion = "1.5.1";
+        public const string LocalVersion = "1.5.2";
         [MenuItem("ParrelSync/Check for update", priority = 20)]
         static void CheckForUpdate()
         {
